174b31e15e34d1406dd5a358f3ac140943c41953
Sagara,Sasikumar <sagaraammu@gmail.com>
2018-10-10 23:28:21 +0530

    Adding desc

--- src/main/java/com/mydevop/app/App.java
+++ src/main/java/com/mydevop/app/App.java
@@ -1,76 +1,22 @@
 package com.mydevop.app;
 import java.util.Scanner;
 import java.text.NumberFormat;
-/**
- * Hello world!
- *
- */
+
 public class App
 {
-
-	public final static String[] units = { "", "One", "Two", "Three", "Four",
-			"Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
-			"Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
-			"Eighteen", "Nineteen" };
-
-	public final static String[] tens = { 
-			"", 		// 0
-			"",		// 1
-			"Twenty", 	// 2
-			"Thirty", 	// 3
-			"Forty", 	// 4
-			"Fifty", 	// 5
-			"Sixty", 	// 6
-			"Seventy",	// 7
-			"Eighty", 	// 8
-			"Ninety" 	// 9
-	};
-	
-	public static String toWords( int n ) {
-		//String result = "" ;
-		//return result.trim() ;
-//		    if(n==0) {
-//		    	 
-//		    	return "Zero";
-//	    }
-			if (n < 0) {
-				return "Error";
-				//return null;
-			}
-
-			if (n < 20) {
-				return units[n];
-	
-			}
-
-			if (n < 100) {
-				return tens[n / 10] + ((n % 10 != 0) ? " " : "") + units[n % 10];
-			}
-
-			if (n < 1000) {
-				return units[n / 100] + " Hundred" + ((n % 100 != 0) ? " " : "") + toWords(n % 100);
-			}
-
-			if (n < 100000) {
-				return toWords(n / 1000) + " Thousand" + ((n % 10000 != 0) ? " " : "") + toWords(n % 1000);
-			}
-
-			if (n < 10000000) {
-				return toWords(n / 100000) + " Lakh" + ((n % 100000 != 0) ? " " : "") + toWords(n % 100000);
-			}
-			
-//			if(n > 9999999999)
-//			{
-//				return "Error";
-//			}
-
+  public final static String[] units = { "", "One", "Two", "Three", "Four","Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve","Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen","Eighteen", "Nineteen" };
+  public final static String[] tens = {	"", "","Twenty", "Thirty","Forty", "Fifty","Sixty", "Seventy","Eighty","Ninety" };
+  public static String toWords( int n ) 
+  {
+	  		if (n < 0) {return "Error";}
+			if (n < 20) {return units[n];}
+			if (n < 100) {return tens[n / 10] + ((n % 10 != 0) ? " " : "") + units[n % 10];}
+			if (n < 1000) {return units[n / 100] + " Hundred" + ((n % 100 != 0) ? " " : "") + toWords(n % 100);}
+			if (n < 100000) {return toWords(n / 1000) + " Thousand" + ((n % 10000 != 0) ? " " : "") + toWords(n % 1000);}
+			if (n < 10000000) {	return toWords(n / 100000) + " Lakh" + ((n % 100000 != 0) ? " " : "") + toWords(n % 100000);}
 			return toWords(n / 10000000) + " Crore" + ((n % 10000000 != 0) ? " " : "") + toWords(n % 10000000);
-			
-			
-		}
-	
-
-	public static void main( String[] args )
+}
+public static void main( String[] args )
     {
     int n;
 	Scanner s=new Scanner(System.in);
--- src/test/java/com/mydevop/app/AppTest.java
+++ src/test/java/com/mydevop/app/AppTest.java
@@ -2,99 +2,48 @@ package com.mydevop.app;
 import org.junit.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
-
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
 	private App app;
-	
-	
 	@Before
-	public void setup() {
-		app=new App();
-	}
-	
-//	@Test
-//	public void appTest1(){
-//		Assert.assertEquals("Zero", app.toWords(0));
-//	}
+	public void setup() {	app=new App();}
 	@Test
-	public void appTest2(){
-		Assert.assertEquals("One",app.toWords(1));
-	}
+	public void appTest2(){Assert.assertEquals("One",app.toWords(1));}
 	@Test
-	public void appTest3(){
-		Assert.assertEquals("Two", app.toWords(2));
-	}
+	public void appTest3(){Assert.assertEquals("Two", app.toWords(2));}
 	@Test
-	public void appTest4(){
-		Assert.assertEquals("Three", app.toWords(3));
-	}
+	public void appTest4(){Assert.assertEquals("Three", app.toWords(3));}
 	@Test
-	public void appTest5(){
-		Assert.assertEquals("Four", app.toWords(4));
-	}
+	public void appTest5(){Assert.assertEquals("Four", app.toWords(4));}
 	@Test
-	public void appTest6(){
-		Assert.assertEquals("Five", app.toWords(5));
-	}
+	public void appTest6(){Assert.assertEquals("Five", app.toWords(5));}
 	@Test
-	public void appTest7(){
-		Assert.assertEquals("Ten", app.toWords(10));
-	}
+	public void appTest7(){Assert.assertEquals("Ten", app.toWords(10));}
 	@Test
-	public void appTest8(){
-		Assert.assertEquals("Twenty Two", app.toWords(22));
-	}
+	public void appTest8(){Assert.assertEquals("Twenty Two", app.toWords(22));}
 	@Test
-	public void appTest9(){
-		Assert.assertEquals("Ninety Nine", app.toWords(99));
-	}
+	public void appTest9(){Assert.assertEquals("Ninety Nine", app.toWords(99));}
 	@Test
-	public void appTest10(){
-		Assert.assertEquals("One Hundred", app.toWords(100));
-	
-	}
+	public void appTest10(){Assert.assertEquals("One Hundred", app.toWords(100));}
 	@Test
-	public void appTest11(){
-		Assert.assertEquals("One Thousand One", app.toWords(1001));
-	}
+	public void appTest11(){Assert.assertEquals("One Thousand One", app.toWords(1001));}
 	@Test
-	public void appTest12(){
-		Assert.assertEquals("Nine Hundred Ninety Nine", app.toWords(999));
-	}
+	public void appTest12(){Assert.assertEquals("Nine Hundred Ninety Nine", app.toWords(999));}
 	@Test
-	public void appTest13(){
-		Assert.assertEquals("One Lakh", app.toWords(100000));
-	}
+	public void appTest13(){Assert.assertEquals("One Lakh", app.toWords(100000));}
 	@Test
-	public void appTest14(){
-		Assert.assertEquals("Ten Lakh", app.toWords(1000000));
-	}
+	public void appTest14(){Assert.assertEquals("Ten Lakh", app.toWords(1000000));}
 	@Test
-	public void appTest15(){
-		Assert.assertEquals("Nine Crore Ninety Nine Lakh Ninety Nine Thousand Nine Hundred Ninety Nine", app.toWords(99999999));
-	}
+	public void appTest15(){Assert.assertEquals("Nine Crore Ninety Nine Lakh Ninety Nine Thousand Nine Hundred Ninety Nine", app.toWords(99999999));}
 	@Test
-	public void appTest16(){
-		Assert.assertEquals("Ninety Nine Crore Ninety Nine Lakh Ninety Nine Thousand Nine Hundred Ninety Nine", app.toWords(999999999));
-	}
+	public void appTest16(){Assert.assertEquals("Ninety Nine Crore Ninety Nine Lakh Ninety Nine Thousand Nine Hundred Ninety Nine", app.toWords(999999999));}
+	@Test	
+	public void appTest17(){Assert.assertEquals("One Hundred Crore", app.toWords(1000000000));}
 	@Test
-	
-	public void appTest17(){
-		Assert.assertEquals("One Hundred Crore", app.toWords(1000000000));
-	}
-	@Test
-	public void numberOutOfRangeReturnsError() {
-		Assert.assertEquals( "Error", app.toWords( -1 )) ;
-	    //Assert.assertEquals( "Error", app.toWords(9999999999)) ;
-
-    }
+	public void numberOutOfRangeReturnsError() {Assert.assertEquals( "Error", app.toWords( -1 )) ;
+ }
 	
 }
